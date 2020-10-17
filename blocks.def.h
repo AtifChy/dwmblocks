9a877d3573cccb85e7f6d31eb73f622f1fa3335b
Atif <atif@archlinux.localdomain>
2020-10-17 17:56:11 +0600

    support for statuscmd added

--- blocks.def.h
+++ blocks.def.h
@@ -1,16 +1,10 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"🐧 ", "uname -r",                                                             	0, 		0},
-        {"📦 ", "checkupdates | wc -l",  							1800, 		0},
-	{"🖥️ ", "$HOME/.local/share/dwmblocks/cpu",					      	2, 		0},
-	{"🌡️ ", "sensors | awk '/Core 0/ {print $3}'",                		          	 5,              0},
-	{"🧠 ", "free -h | awk '/^Mem/ { print $3\"/\"$2 }'",		       			30,	    	0},
-	{"💽 ", "df -h / | awk ' /[0-9]/ {print $3\"/\"$2\"(\"$5\")\"}'", 			1800, 		0},
-	{"📆 ", "$HOME/.local/share/dwmblocks/clock",						60,		0},
+	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
+
+	{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
 };
-
-
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
 static char delim[] = " | ";
