ce76dc081239dbdee859f84553bdf50c39e52dfc
Atif <atif@archlinux.localdomain>
2020-10-24 21:32:04 +0600

    added weather block

--- blocks.h
+++ blocks.h
@@ -2,17 +2,18 @@
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 //	{"🐧 ", "uname -r",                                                    			0, 		1},
-        {"📦 ", "updates", 				  					1800, 		2},
+        {"📦 ", "check-updates", 				  				1800, 		2},
 	{"🖥️ ", "cpu",									      	 2, 	 	 3},
 //	{"🌡️ ", "sensors| awk '/Core 0/ { print $3 }'",             		           	 5,              0},
 	{"🧠 ", "free -h | awk '/^Mem/ { print $3\"/\"$2 }'",		       			3,	    	5},
-	{"💽 ", "df -h / | awk ' /[0-9]/ { print $3\"/\"$2\"(\"$5\")\" }'", 			2000, 		11},
-	{"", "volume }", 									0, 		7},
-	{"📆 ", "clock }",									60,		6},
+//	{"💽 ", "df -h / | awk ' /[0-9]/ { print $3\"/\"$2\"(\"$5\")\" }'", 			2000, 		11},
+//	{"", "network-traffic", 								1, 		14},
+	{"", "volume", 										0, 		7},
+	{"", "weather", 									9000, 		13},
+	{"📆 ", "clock",									30,		6},
 //	{"😃", "emoji", 									0, 		8},
 //	{"📋", "clipboard", 									0, 		9},
 //	{"📴 |", "power", 									0, 		10},
-//	{"", "traffic", 									1, 		12},
 };
 
 
